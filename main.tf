90a2ef69715112b92409a91df548b64e679a0024
joe <kinjoyuji@gmail.com>
2020-11-14 14:50:41 +0900

    fix desired_count

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_ecs_service" "nginx" {
   name                               = "nginx"
   cluster                            = aws_ecs_cluster.this.id
   task_definition                    = aws_ecs_task_definition.nginx.arn
-  desired_count                      = 2
+  desired_count                      = 1
   launch_type                        = "FARGATE"
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
@@ -154,7 +154,7 @@ resource "aws_ecs_service" "rails" {
   name                               = "rails"
   cluster                            = aws_ecs_cluster.this.id
   task_definition                    = aws_ecs_task_definition.rails.arn
-  desired_count                      = 2
+  desired_count                      = 1
   launch_type                        = "FARGATE"
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
